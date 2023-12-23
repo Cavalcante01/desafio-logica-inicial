9a2f1b467e1e7065af31ea59b7ba34e18c7fba79
Cavalcante01 <136639758+Cavalcante01@users.noreply.github.com>
2023-12-23 09:56:17 -0300

    done

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 let nome = "cavalcante";
 let experiencia = 0.20;
 
-if (experiencia <= 1000) {
+if (experiencia <= 1.000) {
     nivel = "Ferro";
-} if (experiencia <= 2000) {
+} if (experiencia <= 2.000) {
     nivel = "Bronze";
-} if (experiencia <= 5000) {
+} if (experiencia <= 5.000) {
     nivel = "Prata";
 } else {
     switch (herói) {
-        case (experiencia <= 7000):
+        case (experiencia <= 7.000):
             nivel = "Ouro";
-        case (experiencia <= 8000):
+        case (experiencia <= 8.000):
             nivel = "Platina";
-        case (experiencia <= 9000):
+        case (experiencia <= 9.000):
             nivel = "Ascendente";
-        case (experiencia <= 10000):
+        case (experiencia <= 10.000):
             nivel = "Imortal";
         case (experiencia <= 10.001):
             nivel = "Radiante";
